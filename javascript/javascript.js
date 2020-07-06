b684395a222c933f3883efd55aefe9045c848723
SteeshMcK <stesha.norstrom@pcc.edu>
2020-07-06 12:47:10 -0700

    add javascript functionality

---
+++ javascript/javascript.js
@@ -0,0 +1,29 @@
+const picPath = "https://source.unsplash.com/featured/?";
+
+var picInput = document.getElementById('picSubject');
+
+document.querySelector('form.picForm').addEventListener('submit', function (e) {
+
+    //prevent the normal submission of the form
+    e.preventDefault();
+
+    const picChoice = picInput.value;   
+
+    const pictures = picPath + picChoice;
+
+    console.log(pictures); 
+
+    showPics(pictures);
+});
+
+
+
+const showPics = (userInput) => {
+    let output = `
+    <div class="picCard">
+    <img src=${userInput} width="400" class="picImage" />
+    <h3>Here you go!</h3>
+    </div>
+`
+    unsplashPics.innerHTML = output
+  }
